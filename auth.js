b0e6772c84557a1f58b8092e9ab2528387dd48af
toddkidneyjourney1 <todd.kidney.journey@gmail.com>
2026-07-05 18:12:38 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,11 @@
 
+// auth.js
+(function() {
+    const password = "YourSecretPassword123"; // Set your password here
+    const input = prompt("Please enter the cast password to continue:");
+    
+    if (input !== password) {
+        alert("Incorrect password. Returning to home.");
+        window.location.href = "index.html";
+    }
+})();
